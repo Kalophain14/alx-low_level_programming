05ada0b82e966bcf13f81d1230f807b110a2133e
Kalophain14 <chueutemosho@gmail.com>
2023-09-07 14:37:01 +0200

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<studio.h>
+/**
+* using put to put a string
+* put automitcally append to a new line "\n"
+*
+* /
+
+int main (void)
+{
+
+  puts("This function doesn't need newline");
+  return (0);
+
+}
