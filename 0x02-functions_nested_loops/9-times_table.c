3f1a1e6132e8ff308c0da3e569a0e5747b3dce4d
Kalophain14 <Chueutemosho@gmail.com>
2023-09-12 14:21:52 -0700

    a + b

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*
+*
+*
+*/
+
+void times_table(void)
+{
+	int a;
+	int t;
+	int n;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (t = 0; t < 10; t++)
+		{
+			n = a * t;
+			if (
