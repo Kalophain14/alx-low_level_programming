792673c7e257f8f03ebec31197932e5bde5a1ffe
Kalophain14 <Chueutemosho@gmail.com>
2023-09-18 16:11:48 -0700

    print *s rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string, in reverse
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int r; /**length 64 characters*/
+	int t; /**for loop reverse*/
+
+	while (*s != '\0') /**Print out the length of the string*/
+	{
+		r++;
+		s++;
+	}
+	for (t = r; t > 0; t--) /**prints out the *s pointer in reverse*/
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
