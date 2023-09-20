5cf44ea9628522c596dda362937717ef239e94f9
Kalophain14 <Chueutemosho@gmail.com>
2023-09-20 07:06:10 -0700

    Hello world appends

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
-*_stract - concatenates two strings
-*@dest: destination of the original pointer src
-*@src: original pointer of the string
-*Return: dest
+* _strcat- function that appends the src to dest for overwriting
+* @dest: destination of the original pointer src
+* @src: original pointer of the string
+* Return: dest
 */
 char *_strcat(char *dest, char *src)
 {
 	int destlen = 0; /**counter for the first loop*/
 	int srclen = 0; /**counter for the second loop*/
-	int r; /**for the loops*/
+	int r;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (r = 0; dest[r] != '\0'; r++)
 		destlen++;
-	for (i = 0; src[i] != '\0'; i++)
+	for (r = 0; src[r] != '\0'; r++)
 		srclen++;
-	for (i = 0; i <= srclen; i++) /**equals to the length of srclen for the \0 to be added*/
-		dest[destlen + i] = src[i] /**copy & concatenates index 0 of src to dest*/
-Return (dest);
+	for (r = 0; r <= srclen; r++) /**equals the length of src array*/
+		dest[destlen + r] = src[r]; /**copy & concatenates index 0 of src to dest*/
+return(dest);
 }
