48c5c899770925ddaa9cb392d2be14c4909548d6
Kalophain14 <Chueutemosho@gmail.com>
2023-09-27 18:45:55 -0700

    sqaure root, new function

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int new_sqrt_recursion(int n, int t);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 #endif
