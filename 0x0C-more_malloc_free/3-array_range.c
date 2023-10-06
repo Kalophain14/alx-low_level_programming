5f892b176f717f8e4cf1701a7ffee4ffedc0b4ec
Kalophain14 <Chueutemosho@gmail.com>
2023-10-06 13:32:14 -0700

    allocate memory of min and max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* array_range - creates an array of intergers
+* @min: minimum value in the array
+* @max:  maximum value in the array
+* Return: new allocated memory
+*/
+
+int *array_range(int min, int max)
+{
+	int *result;
+	int r;
+	int size;
+
+	if (min > max)
+		return (NULL);
+	/**allocating new memory*/
+	size = max - min;
+	result = malloc(sizeof(int) * size + 1);
+	if (result == NULL)
+		return (NULL);
+	for (r = 0; min <= max; r++)
+		result[r] = min++;
+return (result);
+}
