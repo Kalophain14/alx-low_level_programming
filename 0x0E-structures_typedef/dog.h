e2ed256a2b7994a9752d4be795fb1432cb6b30b7
Kalophain14 <Chueutemosho@gmail.com>
2023-10-09 12:40:05 -0700

    ypedef struct assigning dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,20 @@
  * @owner: name of owner
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float  age;
 	char *owner;
-} dog_t;
+};
 
+/**
+ *dog_t - typedef struct dog_t
+ */
+
+typedef struct dog dog_t;
+
+/**prototypes*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
