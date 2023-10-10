515d47d9c1ceff8836d6b5a65b1ca6df78f14366
Kalophain14 <Chueutemosho@gmail.com>
2023-10-10 03:09:31 -0700

    changed to while loops from for loops

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,39 +7,35 @@
  * @s: string to evaluate
  * Return: length of string
  */
-
 int _strlen(char *s)
 {
-	int r;
+	int length = 0;
 
-	for (r = 0; s[r] != '\0'; r++)
+	while (s[length] != '\0')
 	{
+		length++;
 	}
-return (r);
+return (length);
 }
-
 /**
  * *_strcpy - copy the string to pointer src
  * @dest: pointer to destination
  * @src: pointer string to be copied
  * Return: pointer src to dest
  */
-
 char *_strcpy(char *dest, char *src)
 {
-	int length;
-	int t;
+	int length = 0;
 
-	/**copies src to dest*/
-	for (length = 0; src[length] != '\0'; length++)
-	for (t = 0; t < length; t++)
+	while (src[length] != '\0')
 	{
-		dest[t] = src[t];
+		/**char src to dest*/
+		dest[length] = src[length];
+		length++;
 	}
-dest[t] = '\0';
+	dest[length] = '\0';
 return (dest);
 }
-
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
@@ -47,36 +43,45 @@ return (dest);
  * @owner: owner of the dog
  * Return: pointer to the new dog (Success), NULL otherwise
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	int length1;
-	int length2;
+	int length1 = _strlen(name);
+	int length2 = _strlen(owner);
+	int i = 0;
 
-	length1 = _strlen(name);
-	length2 = _strlen(owner);
-
-	dog = malloc(sizeof(dog_t)); /**size of bytes*/
+	/**malloc dog_t struct*/
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
+	/**malloc name*/
 	dog->name = malloc(sizeof(char) * (length1 + 1));
 	if (dog->name == NULL)
 	{
-		free(dog); /**freeing memory*/
+		free(dog);
 		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (length2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
-		free(dog->name); /**assigning free memory*/
+		free(dog->name);
 		return (NULL);
 	}
-_strcpy(dog->name, name);
-_strcpy(dog->owner, owner);
-dog->age = age;
-
+	while (name[i] != '\0')
+	{
+		dog->name[i] = name[i];
+		i++;
+	}
+	dog->name[i] = '\0';
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		dog->owner[i] = owner[i];
+		i++;
+	}
+	dog->owner[i] = '\0';
+	dog->age = age;
 return (dog);
 }
