c05298ae22faa7ad82f434ba528ad06b009992fa
Kalophain14 <Chueutemosho@gmail.com>
2023-10-11 15:32:05 -0700

    compare functions and search for the index in the array function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - seaeches for an interger
+ * @array: array to search from
+ * @size: size of the array
+ * @cmp: pointer function to the array
+ * Return: the index if matches and -1 if not
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1); /*pointer returning an int*/
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		/**cmp searches an intergers and compare of an array fun*/
+		if (cmp(array[i]) == 1)
+			return (i);
+	}
+return (-1);
+}
