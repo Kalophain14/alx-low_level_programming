0fbbb54ca84a525b21da684cb47222209221848b
Kalophain14 <Chueutemosho@gmail.com>
2023-10-12 13:50:49 -0700

    print the pointer string of an unsigned int

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,6 +3,36 @@
 #include "variadic_functions.h"
 
 /**
- *
- *
+ * print_strings - prints strings
+ * @separator: string to be printed
+ * @n: the number of strings passed
  */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list arg;
+	char *str;
+	unsigned int r;
+
+	va_start(arg, n);
+
+	for (r = 0; r < n; r++)
+	{
+		str = va_arg(arg, char *);
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		if (r != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+printf("\n");
+va_end(arg);
+}
