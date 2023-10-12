675d551bd5b345fdd12152ca3d4b175e62663b17
Kalophain14 <Chueutemosho@gmail.com>
2023-10-12 12:57:53 -0700

    print the sum of the variadic functiona rguement in n elements

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum of all parameters
+ * @n: consant number of element
+ * Return: sum of the elements in n
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int r;
+	va_list arg;
+
+	if (n == 0)
+		return (0);
+	va_start(arg, n);
+	for (r = 0; r < n; r++)
+	{
+		sum = sum + va_arg(arg, int);
+	}
+	va_end(arg);
+	return (sum);
+}
