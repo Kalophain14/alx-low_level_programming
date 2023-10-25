b92c7f921fa1daae5014bf3af1236fffef5f453a
‘kalophain14’ <chueutemosho@gmail.com>
2023-10-24 17:23:42 -0700

    Frees a linked list for circular references handling the circular

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,35 +1,72 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a linked list for circular references.
- * @h: Pointer to the first node in the linked list.
- * Return: Number of elements in the freed list.
+ * free_listp2 - Frees a linked list of type listp_t.
+ * @head: Pointer to the head of the list.
+ * Return: No return value.
+ */
+
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *current;
+
+	if (head != NULL)
+	{
+		current = *head;
+		while ((temp = current) != NULL)
+		{
+			current = current->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - Frees a linked list of type listint_t safely.
+ * @h: Pointer to the head of the list.
+ * Return: The number of nodes in the list that was freed.
  */
 
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	listint_t *current, *next;
+	size_t node_count = 0;
+	listp_t *visited_nodes = NULL;
+	listp_t *new_node, *node_iterator;
+	listint_t *current;
 
-	if (!h || !*h)
-		return (0);
-
-	while (*h)
+	while (*h != NULL)
 	{
-		current = *h;
-		next = current->next;
-		len++;
+		new_node = malloc(sizeof(listp_t));
 
-		/**Check for circular reference*/
-		if (current <= next)
+		if (new_node == NULL)
+			exit(98);
+
+		new_node->p = (void *)*h;
+		new_node->next = visited_nodes;
+		visited_nodes = new_node;
+
+		node_iterator = visited_nodes;
+
+		while (node_iterator->next != NULL)
 		{
-			*h = NULL; /**Avoid infinite loop by setting head to NULL */
-			break;
+			node_iterator = node_iterator->next;
+			if (*h == node_iterator->p)
+			{
+				*h = NULL; /**Avoid infinite loop by setting head to NULL*/
+				free_listp2(&visited_nodes);
+				return (node_count);
+			}
 		}
 
+		current = *h;
+		*h = (*h)->next;
 		free(current);
-		*h = next;
+		node_count++;
 	}
 
-	return (len);
+	*h = NULL;
+	free_listp2(&visited_nodes);
+	return (node_count);
 }
