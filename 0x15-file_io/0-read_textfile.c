dd55c7fb5ea33c0d44c7c0553cc22011028f977d
‘kalophain14’ <chueutemosho@gmail.com>
2023-10-30 17:31:56 -0700

    reads a text and write to the standard input file number

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,55 +4,48 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
- * read_textfile - Reads a text file to the POSIX standard output.
- * @filename: The filename to read.
+ * read_textfile - Read a text file and print it to STDOUT.
+ * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
- * Return: number of letters it could read
- *         runs well. 0 otherwise.
+ * Return: The actual number of bytes read and printed (w).
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buffer;
-	ssize_t read_size = 0;
+	char *buf;
+	ssize_t fd, w, t;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-
 	/**Open the file for reading*/
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
 	/**Allocate memory for the buffer*/
-	buffer = malloc(letters);
-	if (!buffer)
+	buf = malloc(letters);
+	if (buf == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-	/**Read and print the file*/
-	read_size = read(fd, buffer, letters);
-	if (read_size == -1)
+
+	/**Read from the file*/
+	t = read(fd, buf, letters);
+	if (t == -1)
 	{
+		free(buf);
 		close(fd);
-		free(buffer);
 		return (0);
 	}
-	/**Close the file*/
+
+	/**Write to STDOUT*/
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
 	close(fd);
-
-	/**Null-terminate the buffer to make it a valid string*/
-	buffer[read_size] = '\0';
-
-	/**Printto standard output*/
-	write(1, buffer, read_size);
-	free(buffer);
-
-/**Return the number of letters read and printed*/
-return (read_size);
+return (w);
 }
-
