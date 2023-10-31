c1e8a0451b8815f9a62040ef2bda620e4a9fcc8d
‘kalophain14’ <chueutemosho@gmail.com>
2023-10-30 17:16:39 -0700

    creates a file if doesnt exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,52 @@
+#include "main.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strlen - Returns the length of a string.
- * @s: The string whose the length will be returned.
- * Return: The length of s.
- */
-int _strlen(char *s)
-{
-	if (*s == 0)
-		return (0);
-	else
-		return (1 + _strlen(s + 1));
-}
-
-/**
- * create_file - Creates a file.
- * @filename: The filename
- * @text_content: The content to write in the file.
- * Return: 1 on success. -1 otherwise.
+ * create_file - Creates a file and writes text content to it.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1. Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = _strlen(text_content);
+	int fd, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
-	if (text_content)
-		write(fd, text_content, len);
-	return (len);
 
+	if (text_content != NULL)
+	{
+		/**Calculate the length of the text content*/
+		for (len = 0; text_content[len]; len++)
+			;
+		/**Open the file, read & create permission doesnt exist.*/
+		/**Set file permissions to 0600 (only owner can read and write*/
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		if (fd == -1)
+			return (-1);
+
+		/**Write the text content to the file*/
+		w = write(fd, text_content, len);
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		close(fd);
+	}
+	else
+	{
+		/**If text_content is NULL, create an empty file.*/
+		fd = open(filename, O_CREAT | O_TRUNC, 0600);
+		if (fd == -1)
+			return (-1);
+		close(fd);
+	}
+return (1);
 }
